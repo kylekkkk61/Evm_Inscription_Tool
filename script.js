e8b2fd325655afe195be3a37aa0c4fbda51c0b5f
kylekkkk <102206550+kylekkkk61@users.noreply.github.com>
2023-11-29 16:39:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
     const executeButton = document.getElementById("execute-button");
+    const pauseInput = document.getElementById("pause-input"); // 新增的秒數輸入框
+
     executeButton.addEventListener("click", function () {
         // Disable the button and update appearance
         executeButton.disabled = true;
         executeButton.innerText = "交易進行中";
         executeButton.style.backgroundColor = "#ccc"; // Set your desired color
 
-        scheduleTransaction();
+        scheduleTransaction(pauseInput.value); // 傳遞秒數給 scheduleTransaction
     });
 });
 
-async function scheduleTransaction() {
+async function scheduleTransaction(pauseSeconds) {
     const rpc = document.getElementById("rpc-input").value;
     const privateKeys = document.getElementById("private-key-input").value.trim().split("\n");
     const nums = parseInt(document.getElementById("nums-input").value);
@@ -29,10 +31,17 @@ async function scheduleTransaction() {
         return;
     }
 
-    for (const privateKey of privateKeys) {
-        const cleanedPrivateKey = privateKey.trim();
-        if (cleanedPrivateKey !== "") {
-            await executeTransaction(cleanedPrivateKey, rpc, nums, data);
+    //
+    const numAccounts = privateKeys.length;
+
+    for (let i = 0; i < numAccounts; i++) {
+        // Print Account information
+        printResult(`---- Account ${i + 1} ----`);
+
+        const privateKey = privateKeys[i].trim();
+        if (privateKey !== "") {
+            await executeTransaction(privateKey, rpc, nums, data);
+            await pauseForSeconds(pauseSeconds); // 在每筆交易之後暫停指定秒數
         }
     }
 
@@ -45,11 +54,9 @@ async function scheduleTransaction() {
     // Add a message indicating all transactions are completed
     printResult("----全部交易已執行完成----");
 }
-
 async function executeTransaction(privateKey, rpc, nums, data) {
     const w3 = new Web3(new Web3.providers.HttpProvider(rpc));
     const fromAddress = w3.eth.accounts.privateKeyToAccount(privateKey).address;
-
 
     try {
         const isNodeListening = await w3.eth.net.isListening();
@@ -57,44 +64,47 @@ async function executeTransaction(privateKey, rpc, nums, data) {
             printResult("網路連接失敗 請重新開啟腳本/更換rpc節點");
             return;
         }
-        printResult(`網路連接成功 開始發送交易 (帳號：${fromAddress})`);
 
         // Ensure that obtaining the nonce is done synchronously
         const nonce = await w3.eth.getTransactionCount(fromAddress);
-        printResult(`當前nonce: ${nonce}, 發送地址: ${fromAddress}`);
 
-        const transactions = [];
 
         for (let i = 0; i < nums; i++) {
+            const gasPrice = await w3.eth.getGasPrice();
+            const adjustedGasPrice = Math.round(gasPrice * 1.1);
+            const chainId = await w3.eth.getChainId();
+            const gas = chainId === 42161 ? 6000000 : 50000;
+
             const transaction = {
                 from: fromAddress,
                 to: fromAddress,
                 value: w3.utils.toWei("0", "ether"),
                 nonce: nonce + i,
-                gas: 50000,
-                gasPrice: await w3.eth.getGasPrice(),
+                gas: gas,
+                gasPrice: adjustedGasPrice,
                 data: w3.utils.toHex(data),
-                chainId: await w3.eth.getChainId(),
+                chainId: chainId,
             };
 
-            transactions.push(transaction);
+            // Print transaction information
+            if (i === 0) {
+                printResult(`網路連接成功 開始發送交易 (帳號：${fromAddress})`);
+                printResult(`當前nonce: ${transaction.nonce}, 發送地址: ${fromAddress}`);
+            }
+
+            const signedTransaction = await w3.eth.accounts.signTransaction(transaction, privateKey);
+
+            try {
+                const receipt = await w3.eth.sendSignedTransaction(signedTransaction.rawTransaction);
+                printResult(`交易成功，Hash: ${receipt.transactionHash}, nonce: ${transaction.nonce}`);
+            } catch (error) {
+                console.error("Error sending transaction:", error);
+                printResult(`交易失敗，nonce: ${transaction.nonce}, Error: ${error.message}`);
+            }
+
+            // Pause for a moment before the next transaction
+            await pauseForSeconds(1); // Adjust the pause duration as needed
         }
-
-        const receipts = await Promise.all(
-            transactions.map(async (transaction) => {
-                const signedTransaction = await w3.eth.accounts.signTransaction(transaction, privateKey);
-
-                return await new Promise((resolve, reject) => {
-                    w3.eth.sendSignedTransaction(signedTransaction.rawTransaction)
-                        .on('receipt', receipt => resolve(receipt))
-                        .on('error', reject);
-                });
-            })
-        );
-
-        receipts.forEach((receipt, index) => {
-            printResult(`交易成功，Hash: ${receipt.transactionHash}, nonce: ${transactions[index].nonce}`);
-        });
 
     } catch (error) {
         console.error("Error checking node connection:", error);
@@ -103,8 +113,13 @@ async function executeTransaction(privateKey, rpc, nums, data) {
 }
 
 
+
 function printResult(message) {
     const resultText = document.getElementById("result-text");
     resultText.value += message + "\n";
     resultText.scrollTop = resultText.scrollHeight;
+}
+
+async function pauseForSeconds(seconds) {
+    return new Promise(resolve => setTimeout(resolve, seconds * 1000));
 }
